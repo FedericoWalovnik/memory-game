7ea1d3432883e3b36ebe61d196d3a3c0f397eebb
FedericoWalovnik <fwalovnik@gmail.com>
2020-09-12 13:30:18 -0300

    Updates

---
+++ precache-manifest.0df88a119dd9b36c486589f266c0fb8a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97d5bab8060926e15f0223a9a4093ef",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "5675c0264b33a8eda95c",
+    "url": "/memory-game/static/css/2.076514a9.chunk.css"
+  },
+  {
+    "revision": "1ca1e56f448cad7f85cc",
+    "url": "/memory-game/static/css/main.4296f6a0.chunk.css"
+  },
+  {
+    "revision": "5675c0264b33a8eda95c",
+    "url": "/memory-game/static/js/2.74d4c84a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.74d4c84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca1e56f448cad7f85cc",
+    "url": "/memory-game/static/js/main.bdb0fc1d.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/memory-game/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/memory-game/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/memory-game/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/memory-game/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/memory-game/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
